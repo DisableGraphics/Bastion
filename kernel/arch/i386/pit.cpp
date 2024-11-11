9355ea6b7fc1731e83f3e29a5235ea83b091c2ac
Noissed <tanimatalis@outlook.com>
2024-11-11 17:55:06 +0100

    flags

--- kernel/arch/i386/pit.cpp
+++ kernel/arch/i386/pit.cpp
@@ -11,46 +11,46 @@ void PIT::init(int freq) {
 	idt.set_handler(0x20, pit_handler);
 	pic.IRQ_clear_mask(0);
 	
-	uint32_t eax, ebx, edx;
+	uint32_t final_freq, ebx, edx;
 
     // Do some checking and determine the reload value
-    eax = 0x10000; // Slowest possible frequency (65536)
+    final_freq = 0x10000; // Slowest possible frequency (65536)
 
     if (freq <= 18) {
     } else if (freq >= 1193181) {
-        eax = 1; // Use the fastest possible frequency
+        final_freq = 1; // Use the fastest possible frequency
     } else {
         // Calculate reload value
-        eax = 3579545;
+        final_freq = 3579545;
         edx = 0;
-        eax /= freq; // eax = 3579545 / frequency, edx = remainder
+        final_freq /= freq; // eax = 3579545 / frequency, edx = remainder
         if ((3579545 % freq) > (3579545 / 2)) {
-            eax++; // Round up if remainder > half
+            final_freq++; // Round up if remainder > half
         }
     }
 
     // Store reload value and calculate actual frequency
-    PIT_reload_value = eax;
-    ebx = eax;
+    PIT_reload_value = final_freq;
+    ebx = final_freq;
 
-    eax = 3579545;
+    final_freq = 3579545;
     edx = 0;
-    eax /= ebx; // eax = 3579545 / reload_value
+    final_freq /= ebx; // eax = 3579545 / reload_value
     if ((3579545 % ebx) > (3579545 / 2)) {
-        eax++; // Round up if remainder > half
+        final_freq++; // Round up if remainder > half
     }
-    IRQ0_frequency = eax;
+    IRQ0_frequency = final_freq;
 
     // Calculate the time in milliseconds between IRQs in 32.32 fixed point
     ebx = PIT_reload_value;
-    eax = 0xDBB3A062; // eax = 3000 * (2^42) / 3579545
+    final_freq = 0xDBB3A062; // 3000 * (2^42) / 3579545
 
-    uint64_t product = (uint64_t)eax * ebx;
-    eax = (product >> 10) & 0xFFFFFFFF; // Get the lower 32 bits after shifting
+    uint64_t product = (uint64_t)final_freq * ebx;
+    final_freq = (product >> 10) & 0xFFFFFFFF; // Get the lower 32 bits after shifting
     edx = product >> 42;                // Get the higher 32 bits after shifting
 
     IRQ0_ms = edx;        // Whole ms between IRQs
-    IRQ0_fractions = eax; // Fractions of 1 ms between IRQs
+    IRQ0_fractions = final_freq; // Fractions of 1 ms between IRQs
 
     // Program the PIT channel (abstracted in this C version)
     idt.disable_interrupts(); // Function to disable interrupts
