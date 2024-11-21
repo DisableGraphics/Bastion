8b6f71026ebee2c3352d1ca4c2b8baaa6f1f0232
DisableGraphics <elchifladod@gmail.com>
2024-11-21 18:11:45 +0100

    nmi + page docs

--- kernel/include/kernel/nmi.hpp
+++ kernel/include/kernel/nmi.hpp
@@ -2,7 +2,15 @@
 
 class NMI {
 	public:
+		/**
+			\brief Disable Non Maskable Interrupts
+			Required for critical sections of code
+			that just can't be interrupted by any means
+		 */
 		void disable();
+		/**
+			\brief Enable Non Maskable Interrupts
+		 */
 		void enable();
 	private:
 };
--- kernel/include/kernel/page.hpp
+++ kernel/include/kernel/page.hpp
@@ -1,11 +1,32 @@
 #pragma once
 #include <stdint.h>
 
+/**
+	\brief Pages manager
+	Manages physical and virtual pages.
+	Implemented as a singleton
+ */
 class PagingManager {
 	public:
+		/**
+			\brief Get singleton instance
+		 */
 		static PagingManager &get();
+		/**
+			\brief Initialise the paging manager
+		 */
 		void init();
+		/**
+			\brief Get a physical address from a virtual address
+			\param virtualaddr Virtual address to get
+		 */
 		void *get_physaddr(void *virtualaddr);
+		/**
+			\brief map a physical page frame in memory to a virtual address
+			\param physaddr Physical address of page frame
+			\param virtualaddr Virtual address of the page
+			\param flags Flags to add to the mapping
+		 */
 		void map_page(void *physaddr, void *virtualaddr, unsigned int flags);
 	private:
 		[[gnu::aligned(4096)]] uint32_t page_directory[1024];
