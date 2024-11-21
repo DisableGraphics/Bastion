cbc119552be5425820de9781816575d8ccbfc357
Noissed <tanimatalis@outlook.com>
2024-11-21 21:25:32 +0100

    panic docs

--- kernel/include/kernel/panic.hpp
+++ kernel/include/kernel/panic.hpp
@@ -2,5 +2,11 @@
 #include "inlineasm.h"
 
 namespace kn {
+	/**
+		\brief kernel panic with a message.
+		Used to signal an unrecoverable error.
+		Disables interruptions and completely halts
+		the computer.
+	*/
 	void panic(const char *str);
 }
--- kernel/kernel/panic.cpp
+++ kernel/kernel/panic.cpp
@@ -1,6 +1,8 @@
 #include <kernel/panic.hpp>
+#include <kernel/interrupts.hpp>
 
 void kn::panic(const char *str) {
 	printf("Kernel panic: %s\n", str);
+	IDT::get().disable_interrupts();
 	halt();
 }
