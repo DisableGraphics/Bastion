024d4243816c895b81f9dcc396ce102305f2cfbf
Noissed <tanimatalis@outlook.com>
2024-11-22 10:52:17 +0100

    better grammar for docs

--- kernel/include/kernel/serial.hpp
+++ kernel/include/kernel/serial.hpp
@@ -1,6 +1,7 @@
 #pragma once
 /**
-	\brief UART serial transmitter/receiver
+	\brief UART serial transmitter/receiver.
+
 	Implemented as a singleton
 */
 class Serial {
@@ -14,36 +15,40 @@ class Serial {
 		 */
 		void init();
 		/**
-			\brief Wether the serial is faulty or not
-			If this returns true you *really* shouldn't use the serial
-			Unless you don't care your data is sent to the shadow
+			\brief Wether the serial is faulty or not.
+
+			If this returns true you *really* shouldn't use the serial.
+			Unless you don't care your data is sent to the shadow.
 			realm or corrupted
 		 */
 		bool is_faulty();
 		/**
-			\brief Wether the serial has received data or not
+			\brief Whether the serial has received data or not
 		 */
 		bool received();
 		/**
 			\brief Read a character from serial.
+
 			This operation blocks the thread of execution 
-			until it receives the data
+			until it receives all the data.
 		 */
 		char read();
 		/**
-			\brief Wether the transmit buffer is empty or not
+			\brief Whether the transmit buffer is empty or not
 		 */
 		bool is_transmit_empty();
 		/**
 			\brief Write a byte to the serial.
+			
 			This operation blocks the calling thread of execution.
 		 */
 		void write(char a);
 		/**
 			\brief Write an array of bytes to the serial.
+
 			This operation blocks the calling thread of execution.
-			Depending on the size of the array this will take an eternity.
-			So use this sporadically at best.
+			Depending on the size of the array this will take an eternity,
+			so use this sporadically at best.
 		 */
 		void print(const char *str);
 	private:
