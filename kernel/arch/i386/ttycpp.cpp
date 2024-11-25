ed1c8d8c1d798b9404d2889ccc4d7c8382c74a04
Noissed <tanimatalis@outlook.com>
2024-11-25 21:57:35 +0100

    finished the process of C++ ification

--- kernel/arch/i386/ttycpp.cpp
+++ kernel/arch/i386/ttycpp.cpp
@@ -1,24 +1,51 @@
 #include <kernel/tty.hpp>
-#include <kernel/tty.h>
+#include "vga.hpp"
+#include <string.h>
 
 void TTY::init() {
-    terminal_initialize();
-
+    terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = VGA_MEMORY;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
 }
 
 void TTY::putchar(char c) {
-    terminal_putchar(c);
+    unsigned char uc = c;
+	if(uc == '\n'){
+		terminal_column = 0;
+		if(++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+		return;
+	}
+	putentryat(uc, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
 }
 
 void TTY::write(const char* data, size_t size) {
-    terminal_write(data, size);
+    for (size_t i = 0; i < size; i++)
+		putchar(data[i]);
 }
 
 void TTY::writestring(const char* data) {
-    terminal_writestring(data);
+    write(data, strlen(data));
 }
 
 TTY& TTY::get() {
 	static TTY instance;
 	return instance;
+}
+
+void TTY::putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
 }
--- kernel/include/kernel/tty.hpp
+++ kernel/include/kernel/tty.hpp
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 
 // TTY
 class TTY {
@@ -10,4 +11,15 @@ class TTY {
         void writestring(const char* data);
 	private:
 		TTY(){};
+
+		void putentryat(unsigned char c, uint8_t color, size_t x, size_t y);
+
+		const size_t VGA_WIDTH = 80;
+		const size_t VGA_HEIGHT = 25;
+		uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
+
+		size_t terminal_row;
+		size_t terminal_column;
+		uint8_t terminal_color;
+		uint16_t* terminal_buffer;
 };
