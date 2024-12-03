f6f42ef2936f9ad1e8e0d32322d7e1e26889d067
Noissed <tanimatalis@outlook.com>
2024-12-03 13:58:33 +0100

    docs

--- kernel/include/kernel/sync/semaphore.h
+++ kernel/include/kernel/sync/semaphore.h
@@ -6,11 +6,22 @@ extern "C" {
 typedef struct {
     void* count; // Opaque pointer to internal semaphore data
 } sem_t;
-
+/**
+	\brief Initialise the semaphore.
+	\param initial_count Initial semaphore count.
+ */
 void semaphore_init(sem_t* sem, int initial_count);
+/**
+	\brief Destroy semaphore.
+ */
 void semaphore_destroy(sem_t* sem);
-
+/**
+	\brief Wait until semaphore is free.
+ */
 void semaphore_wait(sem_t* sem);
+/**
+	\brief Signal the semaphore that it is free.
+ */
 void semaphore_signal(sem_t* sem);
 
 #ifdef __cplusplus
