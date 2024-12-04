7b3d717b55e2cb8db59b16bc64fe62451aebe79c
DisableGraphics <elchifladod@gmail.com>
2024-12-04 11:37:11 +0100

    qemu mods

--- qemu.sh
+++ qemu.sh
@@ -3,7 +3,7 @@ set -e
 . ./iso.sh
 
 
-OPTIONS="-cdrom bastion.iso -serial file:bastion.serial -monitor stdio -m 344M -hda disk.img"
+OPTIONS="-cdrom bastion.iso -serial file:bastion.serial -monitor stdio -m 344M -hda disk.img -boot menu=on"
 DEBUG_OPTIONS="-s -S"
 
 if [ "$1" == "debug" ]; then
