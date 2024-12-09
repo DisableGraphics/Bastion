eb85af2abe79e895d9cf28676231e2cd88bbb251
Noissed <tanimatalis@outlook.com>
2024-12-09 22:38:03 +0100

    continue work on memory mapped io

--- kernel/include/kernel/kernel/mmio.hpp
+++ kernel/include/kernel/kernel/mmio.hpp
@@ -5,5 +5,7 @@ class MMIO {
 		static MMIO& get();
 		void init(used_region* regions, size_t nregions);
 	private:
+		void init_page_tables(void* origin, size_t n_page_tables);
 		MMIO(){}
+		page_t *page_tables;
 };
--- kernel/kernel/mmio.cpp
+++ kernel/kernel/mmio.cpp
@@ -10,10 +10,24 @@ void MMIO::init(used_region *regions, size_t nregions) {
 	used_region &last_region = regions[nregions-1];
 	size_t npages = (reinterpret_cast<size_t>(last_region.end) 
 		- reinterpret_cast<size_t>(last_region.begin)) / PAGE_SIZE;
+	size_t npage_tables = npages / PAGE_SIZE;
+
+	init_page_tables(last_region.begin, npage_tables);
+
 	PagingManager &pm = PagingManager::get();
 	for(size_t i = 0; i < npages; i++) {
 		uintptr_t ptr = reinterpret_cast<uintptr_t>(last_region.begin) + PAGE_SIZE*i;
 		void *raw_addr = reinterpret_cast<void*>(ptr);
 		pm.map_page(raw_addr, raw_addr, CACHE_DISABLE | READ_WRITE);
+	}
+}
+
+void MMIO::init_page_tables(void* origin, size_t n_page_tables) {
+	page_tables = reinterpret_cast<page_t*>(MemoryManager::get().alloc_pages(n_page_tables));
+	size_t addr = reinterpret_cast<size_t>(page_tables);
+	PagingManager &pm = PagingManager::get();
+	size_t originp = reinterpret_cast<size_t>(origin);
+	for(size_t i = 0; i < n_page_tables; i++) {
+		pm.new_page_table(reinterpret_cast<void*>(addr + i*PAGE_SIZE), reinterpret_cast<void*>(originp + REGION_SIZE*i));
 	}
 }
