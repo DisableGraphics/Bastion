aa565ddc7e51f2ed4dd7582bb7cfe0da597bd521
DisableGraphics <elchifladod@gmail.com>
2025-03-31 21:46:36 +0200

    simd draw rectangle (WIP)

--- kernel/kernel/drivers/vesa/vesahelp.c
+++ kernel/kernel/drivers/vesa/vesahelp.c
@@ -14,7 +14,7 @@ void fast_clear(uint32_t color, uint8_t* fb, size_t pixel_count) {
 void draw_rectangle(int x1, int y1, int x2, int y2, uint32_t packed_color, uint8_t* backbuffer, size_t* row_pointers, uint32_t depth_disp) {  
     __m128i color_vec;
 	color_vec = _mm_set1_epi32(packed_color);
-    size_t bytes_pp = 4;
+    size_t bytes_pp = sizeof(uint32_t);
     
     for (int y = y1; y <= y2; y++) {
         uint8_t* ptr = backbuffer + row_pointers[y] + (x1 << depth_disp);
@@ -28,7 +28,7 @@ void draw_rectangle(int x1, int y1, int x2, int y2, uint32_t packed_color, uint8
         }
         
         // Process 4 pixels at a time (16 bytes for 32bpp)
-        int simd_pixels = pixels_left >> 2;
+        int simd_pixels = pixels_left >> 4;
         for (int i = 0; i < simd_pixels; i++) {
             _mm_store_si128((__m128i*)(ptr), color_vec);
             ptr += bytes_pp << 2;
