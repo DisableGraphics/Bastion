d49b93c0ecac01fdbcfefa963357795e6b45851b
DisableGraphics <elchifladod@gmail.com>
2025-11-05 22:05:18 +0100

    ipc msg

--- include/ipc.h
+++ include/ipc.h
@@ -6,6 +6,30 @@ extern "C" {
 #endif
 #include <stdint.h>
 #include <stddef.h>
+// Error definitions
+/// No error
+#define EOK 0
+/// No destination
+#define ENODEST 1
+/// Invalid operation
+#define EINVALOP 2
+
+// Flags
+/// Paging flags
+#define IPC_FLAG_NONE 0
+//// Share a page with destination
+#define IPC_FLAG_SHARE_PAGE (1 << 0)
+//// Transfer page to destination
+#define IPC_FLAG_GRANT_PAGE (1 << 1)
+/// Synchronisation flags
+//// Make the IPC call non blocking (by default IPC calls are blocking). Returns ENODEST if there is no receiver or sender.
+#define IPC_FLAG_NONBLOCKING (1 << 8)
+/// Port management flags. (If both are active at the same time the IPC call will return EINVALOP)
+/// Also these options are only to be used with sys_ipc_send by the port owner. If they aren't the resulting operation will return EINVALOP
+//// Grant rights to all port holders that aren't the port owner.
+#define IPC_FLAG_GRANT_RIGHTS (1 << 16)
+//// Revoke rights from all port holders that aren't the port owner.
+#define IPC_FLAG_REVOKE_RIGHTS (1 << 17)
 
 typedef int16_t port_t;
 typedef uint32_t flags_t;
