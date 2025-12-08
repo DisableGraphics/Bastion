e7737c302181308d365607804e0071672b9142b8
DisableGraphics <elchifladod@gmail.com>
2025-12-08 22:18:09 +0100

    kernel api for 2 syscalls

--- include/ipc.h
+++ include/ipc.h
@@ -23,15 +23,42 @@ extern "C" {
 // Flags
 /// Paging flags
 #define IPC_FLAG_NONE 0
-//// Share a page with destination
+//// Share a page with destination. Destination and source can both access the page.
 #define IPC_FLAG_SHARE_PAGE (1 << 0)
-//// Transfer page to destination
+//// Transfer page to destination. Destination owns the page from now on and the source cannot access it in any way.
 #define IPC_FLAG_GRANT_PAGE (1 << 1)
-//// The message to be sent is a syscall message. Particularly: last two 
-#define IPC_FLAG_SYSCALL_MSG (1 << 2)
+//// Take page from destination. Destination will not be allowed to use that page anymore. Effectively source steals from destination.
+#define IPC_FLAG_REVOKE_PAGE (1 << 2)
+
+/// Process management flags. All operations can only be used on sys_ipc_send() unless otherwise noted.
+//// Creates a new process and returns the port of the process into source.
+//// The new process will share the same address space as the process that created it, so it is effectively a thread of the parent.
+#define IPC_FLAG_CREATE_PROCESS (1 << 4)
+//// Destroys the process behind the dest port. Source process has to have permissions to kill the destination process.
+//// Also note that this message is mandatory when finishing a process.
+#define IPC_FLAG_KILL_PROCESS (1 << 5)
+//// Creates a new address space for the port owner process.
+#define IPC_FLAG_NEW_ADDR_SPACE_PROC (1 << 6)
+//// Waits for the port owner to close the port. As ports are freed when a process is closed, this can also work as a substitute of wait() in POSIX systems.
+//// Can only be used in sys_ipc_recv()
+#define IPC_FLAG_WAIT_PROCESS (1 << 7)
+
 /// Synchronisation flags
 //// Make the IPC call non blocking (by default IPC calls are blocking). Returns ENODEST if there is no receiver or sender.
 #define IPC_FLAG_NONBLOCKING (1 << 8)
+
+/// Interrupt management flags. Unless noted, any flags must only be used on sys_ipc_send()
+//// Register to an interrupt. Interrupt number must be added in field value0 of the ipc message.
+#define IPC_FLAG_INT_REGISTER (1 << 12)
+//// Acknowledge interrupt. If interrupt is not acknowledged, the interrupt source will not emit more interrupts until it is acknowledged. 
+#define IPC_FLAG_INT_ACK (1 << 13)
+//// Request I/O port acccess. The number of the requested IO port goes into value0. This is only valid for CPU architectures that have I/O ports like x86.
+#define IPC_FLAG_PORT_IO (1 << 14)
+//// Register port as syscall handler. 
+//// When an unknown syscall is emitted and the process has a syscall handler, instead of returning immediately from kernel, a message will be sent to the
+//// registered syscall handler to process. Until it has been fully processed, the source process of the syscall will remain blocked.
+#define IPC_FLAG_SYSCALL_REGISTER (1 << 15)
+
 /// Port management flags. (If the first two are active at the same time the IPC call will return EINVALOP)
 /// Also these options are only to be used with sys_ipc_send by the port owner. If they aren't the resulting operation will return EINVALOP
 //// Grant rights to all port holders that aren't the port owner.
@@ -42,7 +69,7 @@ extern "C" {
 #define IPC_FLAG_TRANSFER_PORT (1 << 18)
 
 // Rights
-// Rights are granted/revoked in the "value" field of the message.
+// Rights are granted/revoked using the "value0" field of the message.
 // On creation of a port, only the send right is active by default.
 /// Allows other processes to send messages via the port (active by default)
 #define IPC_RIGHT_SEND (1 << 0)
@@ -54,6 +81,8 @@ extern "C" {
 #define IPC_RIGHT_KILL (1 << 3)
 /// Allows other processes to grant and revoke port rights.
 #define IPC_RIGHT_MODIF_RIGHTS (1 << 4)
+/// Allows other processes to grant, revoke and share pages.
+#define IPC_RIGHT_PAGING (1 << 5)
 
 // Some type definitions for messages
 typedef int16_t port_t;
